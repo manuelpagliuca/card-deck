2f6290ccb664162191c10ce939f9b2883eaababb
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2024-01-25 16:07:52 +0100

    expanding the tests also for IO operation

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,45 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Expected deck length of 20, but got %v", len(d))
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	if loadedDeck[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", loadedDeck[0])
+	}
+
+	if loadedDeck[len(loadedDeck)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", loadedDeck[len(loadedDeck)-1])
+	}
+
+	os.Remove(("_decktesting"))
 }
